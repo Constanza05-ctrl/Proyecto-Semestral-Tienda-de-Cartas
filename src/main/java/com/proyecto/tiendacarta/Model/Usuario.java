192340f7e63047b3032bf8e5d91453efc6d44c11
pipe <feli.acuna@duocuc.cl>
2026-04-06 23:18:38 -0400

    se sube una version con el model de usuario

---
+++ src/main/java/com/proyecto/tiendacarta/Model/Usuario.java
@@ -0,0 +1,34 @@
+package com.proyecto.tiendacarta.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data   
+@Entity
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+
+    private String nombre;
+    /*apellido paterno */
+    private String apellidoP;
+    /*apellido materno */
+    private String apellidoM;
+    /*nombre de usuario */
+    private String nombreUser;
+    private String correo;
+    private String password;
+    private Number edad; 
+    private String genero;
+    /*tipo usuario como si es vendedor o es el comrpador */
+    private String tipoUser;
+
+
+
+}
